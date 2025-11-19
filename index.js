5ed8e0d68173f8ed2894b8f5f981dacce1e80ce6
Brunda D R <brundadr315@gmail.com>
2025-11-19 20:43:20 +0530

    initial commit

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const app = express();
 
 // Middleware - Configure CORS properly
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true
+  origin: "*",
+ credentials: false
 }));
 app.use(express.json());
 
